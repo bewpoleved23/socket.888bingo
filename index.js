85b1ae62f89b2225d8e4fd21bd9d196e320955bc
ced <cedrickcampoto@yahoo.com>
2022-09-24 10:29:20 +0800

    emit admin

--- index.js
+++ index.js
@@ -26,7 +26,15 @@ const server = http.createServer(app).listen(port, function(err){
 
 const io = new socket.Server(server, {
     cors:{
-        origin: ["http://localhost:5000","https://uat.888bingo.ph","https://888bingo.ph"],
+        origin: [
+            "http://localhost:5000",
+            "https://uat.888bingo.ph",
+            "https://888bingo.ph",
+
+            "http://localhost:9821",
+            "https://admintest.888bingo.ph",
+            "https://admin.888bingo.ph"
+        ],
         methods: ["GET", "POST"] ,
         credentials: true
     }
@@ -37,37 +45,10 @@ io.on('connection',(socket)=>{
     socket.emit('message', {message:`connected to socket server with id ${socket.id}`});
 });
 
-io.of('/lobby').on('connection',(socket)=>{
-    try {
 
- 
-        const cookie = socket.handshake.headers.cookie;
+let countConnectedLobby = {};
+let countGamePlayed = {};
 
-        
-        const parsed = cookie?cookieParser.parse(cookie):{};
-        socket.emit('message', {message:`connected to socket server with id ${socket.id}`, cookie:socket.handshake.headers.cookie});
-        socket.on('/update-jackpot', async (arg, callback)=>{
-            let jackpot = await JackpotController.getMainJackpot();
-
-            if(jackpot.status){
-
-                callback({data:jackpot.data.data.data});
-            } else {
-                callback({data:0});
-            }
-        });
-        socket.on('/update-player', (arg, callback)=>{
-
-        });
-
-        socket.on('disconnect',function(){
-            console.log('disconnected');
-        })
-
-    }catch(err){
-        console.log(63, err);
-    };
-});
 
 io.of('/admin').on('connection',(socket)=>{
     
@@ -86,6 +67,63 @@ io.of('/admin').on('connection',(socket)=>{
         console.log(63, err);
     };
 });
+io.of('/lobby').on('connection',(socket)=>{
+    try {
+
+        const cookie = socket.handshake.headers.cookie;
+        const parsed = cookie?cookieParser.parse(cookie):{};
+
+        socket.emit('message', {message:`connected to socket server with id ${socket.id}`, cookie:socket.handshake.headers.cookie});
+        socket.on('/update-jackpot', async (arg, callback)=>{
+            let jackpot = await JackpotController.getMainJackpot();
+
+            if(jackpot.status){
+                countConnectedLobby[socket.id] = 1;
+
+                callback({data:jackpot.data.data.data});
+            } else {
+                callback({data:0});
+            }
+        });
+        socket.on('/update-player', (arg, callback)=>{
+
+        });
+
+        socket.on('disconnect',function(){
+            delete countConnectedLobby[socket.id];
+        })
+
+    }catch(err){
+        console.log(63, err);
+    };
+});
+io.of('/game').on('connection',(socket)=>{
+    try {
+
+        const cookie = socket.handshake.headers.cookie;
+        const parsed = cookie?cookieParser.parse(cookie):{};
+
+        socket.emit('message', {message:`connected to socket server with id ${socket.id}`, cookie:socket.handshake.headers.cookie});
+        socket.on('/init', async (arg)=>{
+            if(arg.username){
+            } else {
+                
+            };
+            if(!countGamePlayed[socket.id]){
+                countGamePlayed[socket.id] = arg.game;
+            };
+        });
+
+        socket.on('disconnect',function(){
+            if(countGamePlayed[socket.id]){
+                delete countGamePlayed[socket.id];
+            } else {};
+        })
+
+    }catch(err){
+        console.log(63, err);
+    };
+});
 
 app.post('/send',function(req, res, next){
     const nsp = req.query.nsp;
@@ -99,3 +137,21 @@ app.post('/send',function(req, res, next){
         res.json({message:'success'});
     }
 });
+
+setInterval(()=>{
+    io.of('admin').emit('/updateGameUsers',Object.values(countGamePlayed).reduce((accu, game)=>{
+        if(accu[game]){
+            accu[game] += 1;
+        } else {
+            accu[game] = 1;
+        };
+        return accu;
+    },{}));
+
+    io.of('admin').emit('/updateLobbyUsers',Object.values(countConnectedLobby).reduce((accu, iter)=>{
+        accu += iter;
+        return accu;
+    },0));
+},3000);
+
+
